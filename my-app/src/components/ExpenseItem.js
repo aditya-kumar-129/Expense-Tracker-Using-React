8019f0faba05e2538ecaaa21404beec892950bbc
aditya-kumar-129 <adityakumar93340@gmail.com>
2021-12-10 11:24:50 +0530

    Learnt how to spilit the child component to further breakdown

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,15 +1,18 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem (props) {
-  // const expenseDate = new Date(2021,2,28);
-  // const expenseTitle = 'Car Insurance';
-  // const expenseAmount = 294.67;
   
   return(
     <div className="expense-item">
       {/* <div>{expenseDate.toISOString()}</div> */}
       {/* <div>{expenseDate.toString()}</div> */}
-      <div>{props.date.toDateString()}</div>
+      {/* https://flaviocopes.com/how-to-get-month-from-javascript-date/ */}
+      {/* https://stackoverflow.com/questions/6002254/get-the-current-year-in-javascript */}
+      {/* https://www.w3schools.com/js/js_date_methods.asp */}
+      
+      <ExpenseDate date = {props.date}/>
+      <div></div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
