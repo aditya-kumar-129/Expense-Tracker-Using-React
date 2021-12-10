7bb6e75bcb9725764bf802363ba893c6e22ff8ed
aditya-kumar-129 <adityakumar93340@gmail.com>
2021-12-10 14:10:43 +0530

    Learnt about props.children

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,10 +1,11 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 
 function ExpenseItem (props) {
   
   return(
-    <div className="expense-item">
+    <Card className="expense-item">
       {/* <div>{expenseDate.toISOString()}</div> */}
       {/* <div>{expenseDate.toString()}</div> */}
       {/* https://flaviocopes.com/how-to-get-month-from-javascript-date/ */}
@@ -12,12 +13,11 @@ function ExpenseItem (props) {
       {/* https://www.w3schools.com/js/js_date_methods.asp */}
       
       <ExpenseDate date = {props.date}/>
-      <div></div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div> 
+    </Card> 
   )
 }
 
