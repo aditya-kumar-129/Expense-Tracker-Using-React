ce752335c20df3d69add178cf576eef588d495d0
aditya-kumar-129 <adityakumar93340@gmail.com>
2021-12-10 21:16:33 +0530

    Learnt about useState in react using a button to respond and thus change the title when clicked

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import Expenses from "./components/Expense/Expenses";
-function App() {
+const App = () => {
   const expenses = [
     {id: "e1", title: "Toilet Paper", amount: 94.12, date: new Date(2020, 7, 14)},
     {id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12)},
--- my-app/src/components/Expense/ExpenseItem.js
+++ my-app/src/components/Expense/ExpenseItem.js
@@ -1,7 +1,14 @@
+import {useState} from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+
 const ExpenseItem = (props) => {
+  const [title,setTitle] =useState(props.title);   const change_title_clickHandler = (e) => {
+    console.log(title);
+    setTitle('Change the title');
+    e.preventDefault();
+  };
   return (
     <Card className="expense-item">
       {/* <div>{expenseDate.toISOString()}</div> */}
@@ -12,8 +19,9 @@ const ExpenseItem = (props) => {
 
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={change_title_clickHandler}>Change Title</button>
       </div>
     </Card>
   );
