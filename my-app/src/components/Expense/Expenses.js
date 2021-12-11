e94d185e25908810bab83e9e9bc1ddd3097e22cb
aditya-kumar-129 <adityakumar93340@gmail.com>
2021-12-11 16:52:44 +0530

    Learn how to dynamcally traverse each objects that are present in the array using map function.Also learnt how to dynamically add the object to array using useState (IMPORTANT PART) (it works littile different in this case.

--- my-app/src/components/Expense/Expenses.js
+++ my-app/src/components/Expense/Expenses.js
@@ -12,35 +12,24 @@ const Expenses = (props) => {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-  
+
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
+      {props.expenses.map((single_object) => (
+        <ExpenseItem
+          title={single_object.title}
+          amount={single_object.amount}
+          date={single_object.date}
+        />
+      ))}
     </Card>
   );
 };
 
 export default Expenses;
+
+// doubt:- generally we use ()=>{} pattern for arrow function but in this case we are using ()=>() What is the reason behind this??
