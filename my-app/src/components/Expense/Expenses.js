7c9e5a0d72f1cbafc8d563be7b5eaa55f545191a
aditya-kumar-129 <adityakumar93340@gmail.com>
2021-12-11 15:50:11 +0530

    Made a Filter(non-working) UI and removed the button that is used to change the title of the heading as it was used just to learn useState.Also made the button of rounded border

--- my-app/src/components/Expense/Expenses.js
+++ my-app/src/components/Expense/Expenses.js
@@ -1,12 +1,24 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+import { useState } from "react";
 
 // If you ever get stuck for finding the path then type ./
 // as soon as you type / Visual studio code will show you the all the js file present in that directry
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  
   return (
     <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
