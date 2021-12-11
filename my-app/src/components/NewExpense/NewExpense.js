f03087cb9896f3d5e06ddd0a7d0a773cc921d388
aditya-kumar-129 <adityakumar93340@gmail.com>
2021-12-11 21:57:15 +0530

    Added a cancel button on the newExpense Form and add some dynamic funcnatioanlity to the cancel and submit button and when user clicks on the cancel button then a new UI has been added which shows a button to add expence items

--- my-app/src/components/NewExpense/NewExpense.js
+++ my-app/src/components/NewExpense/NewExpense.js
@@ -1,20 +1,46 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
-import './NewExpense.css';
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
-  const newExpenseDataHandler = (newExpenseData) =>{
+
+  const [openNewExpenseBox, closeNewExpenseBox] = useState(false);    //Initial value is false because we want the user to click on 'add more expense' button when the user opens the web
+
+  const newExpenseDataHandler = (newExpenseData) => {
     const expenseData = {
       ...newExpenseData,
-      id:Math.random().toString()
-    }
-    props.onNewExpense(expenseData)
-    // console.log(expenseData);
+      id: Math.random().toString(),
+    };
+    props.onNewExpense(expenseData);
+    closeNewExpenseBox(false); // as soon as submit button is clicked expense box get closed
+  };
+
+  const openExpenseForm = () => {
+    closeNewExpenseBox(true);
+  };
+
+  const closeNewExpense = () => {
+    closeNewExpenseBox(false);
+  };
+
+  // I have used the concept of storing the value the JSX code in a Variable
+  let contentToBeDispalyed;
+  if (openNewExpenseBox === true) {   // if the variable is true it means that the user has to enter the input
+    contentToBeDispalyed = (
+      <ExpenseForm
+        onNewExpenseData={newExpenseDataHandler}
+        onCancel={closeNewExpense}
+      />
+    );
+  } else {
+    contentToBeDispalyed = (
+      <button onClick={openExpenseForm}>
+        Add new Expense
+      </button>
+    );
   }
-  return (
-    <div className="new-expense">
-      <ExpenseForm onNewExpenseData={newExpenseDataHandler}/>
-    </div>
-  )
+
+  return <div className="new-expense">{contentToBeDispalyed}</div>;
 };
 
 export default NewExpense;
