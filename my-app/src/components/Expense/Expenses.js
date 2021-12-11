2e2b9f17afe277fcd1b164e9c103614f5f025eba
aditya-kumar-129 <adityakumar93340@gmail.com>
2021-12-11 18:50:51 +0530

    Learnt the various ways to use conditional statement

--- my-app/src/components/Expense/Expenses.js
+++ my-app/src/components/Expense/Expenses.js
@@ -18,27 +18,43 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  // We can store JSX code in a variable like the one below done
+  let expenseContent = <p>No Money spent in this month</p>;
+
+  if (filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map((single_object) => (
+      <ExpenseItem
+        key={single_object.id}
+        title={single_object.title}
+        amount={single_object.amount}
+        date={single_object.date}
+      />
+    ));
+  }
+
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.map((single_object) => (
-        <ExpenseItem
-          key={single_object.id}
-          title={single_object.title}
-          amount={single_object.amount}
-          date={single_object.date}
-        />
-      ))}
+      {expenseContent}
+      {/* below method can also be used as the JS logic evaluated the logic like if the first part of the condition is correct then render the part after the && */}
+      {/* {filteredExpenses.length===0 && <p>No Money spent in this month</p>} */}
+      {/* {filteredExpenses.length>0 && filteredExpenses.map((single_object) => (
+      <ExpenseItem
+        key={single_object.id}
+        title={single_object.title}
+        amount={single_object.amount}
+        date={single_object.date}
+      />
+    ))} */}
     </Card>
   );
 };
 
 export default Expenses;
-// Have a llok on the below link in order to remove the link 
+// Have a llok on the below link in order to remove the link
 // https://stackoverflow.com/questions/28329382/understanding-unique-keys-for-array-children-in-react-js
-
 
 // doubt:- generally we use ()=>{} pattern for arrow function but in this case we are using ()=>() What is the reason behind this??
