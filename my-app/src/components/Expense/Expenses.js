db120fa00dd52b43e783e041b5c5696e284bcaee
aditya-kumar-129 <adityakumar93340@gmail.com>
2021-12-11 19:30:44 +0530

    Learnt a new way to handle conditional statement (When all the JSX code to be returned is dependent on the conditional statement

--- my-app/src/components/Expense/Expenses.js
+++ my-app/src/components/Expense/Expenses.js
@@ -1,8 +1,8 @@
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 import { useState } from "react";
+import ExpensesList from "./ExpensesList";
 
 // If you ever get stuck for finding the path then type ./
 // as soon as you type / Visual studio code will show you the all the js file present in that directry
@@ -18,19 +18,19 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  // We can store JSX code in a variable like the one below done
-  let expenseContent = <p>No Money spent in this month</p>;
+      // // We can store JSX code in a variable like the one below done
+      // let expenseContent = <p>No Money spent in this month</p>;
 
-  if (filteredExpenses.length > 0) {
-    expenseContent = filteredExpenses.map((single_object) => (
-      <ExpenseItem
-        key={single_object.id}
-        title={single_object.title}
-        amount={single_object.amount}
-        date={single_object.date}
-      />
-    ));
-  }
+      // if (filteredExpenses.length > 0) {
+      //   expenseContent = filteredExpenses.map((single_object) => (
+      //     <ExpenseItem
+      //       key={single_object.id}
+      //       title={single_object.title}
+      //       amount={single_object.amount}
+      //       date={single_object.date}
+      //     />
+      //   ));
+      // }
 
   return (
     <Card className="expenses">
@@ -38,7 +38,8 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {expenseContent}
+      <ExpensesList filteredContent={filteredExpenses}/>
+      {/* {expenseContent} */}
       {/* below method can also be used as the JS logic evaluated the logic like if the first part of the condition is correct then render the part after the && */}
       {/* {filteredExpenses.length===0 && <p>No Money spent in this month</p>} */}
       {/* {filteredExpenses.length>0 && filteredExpenses.map((single_object) => (
@@ -54,7 +55,7 @@ const Expenses = (props) => {
 };
 
 export default Expenses;
-// Have a llok on the below link in order to remove the link
+// Have a look on the below link in order to remove the the key error form console
 // https://stackoverflow.com/questions/28329382/understanding-unique-keys-for-array-children-in-react-js
 
 // doubt:- generally we use ()=>{} pattern for arrow function but in this case we are using ()=>() What is the reason behind this??
