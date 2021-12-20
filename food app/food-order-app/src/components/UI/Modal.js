4baa5774c65f321af8f911a34a229237a35f84c6
aditya-kumar-129 <adityakumar93340@gmail.com>
2021-12-20 20:21:24 +0530

    Modal and Backdrop created

---
+++ food app/food-order-app/src/components/UI/Modal.js
@@ -0,0 +1,31 @@
+import styles from "./Modal.module.css";
+import { Fragment } from "react";
+import ReactDom from "react-dom";
+
+const BackDrop = () => {
+  return <div className={styles.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDom.createPortal(<BackDrop />, portalElement)}
+      {ReactDom.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
